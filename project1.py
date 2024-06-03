2399df4ede8b72d4ade68efbad22fe37705e2b41
NasirBaloch123 <167631920+NasirBaloch123@users.noreply.github.com>
2024-06-03 19:56:57 +0500

    project-1

---
+++ project1.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name generator")
+city = str(input("enter the city you grew up in"))
+pet = str(input("enter the name of your pet"))
+print("your band name could be" + city + " " + pet)
