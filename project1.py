ff433113b0da472dbd277ed402b0ddb3f2f2b1ce
NasirBaloch123 <167631920+NasirBaloch123@users.noreply.github.com>
2024-07-16 18:37:05 +0500

    Caesar Cipher Project

--- project1.py
+++ project1.py
@@ -1,4 +1,31 @@
-print("Welcome to the band name generator")
-city = str(input("enter the city you grew up in"))
-pet = str(input("enter the name of your pet"))
-print("your band name could be" + city + " " + pet)
+favorite_places = {
+    "Alice": ["Paris", "New York", "Tokyo"],
+    "Bob": ["London", "Berlin", "Madrid"],
+    "Charlie": ["Sydney", "Melbourne", "Brisbane"]
+}
+
+
+for person, places in favorite_places.items():
+    print(f"{person} likes the following places:")
+    for place in places:
+        print(f"- {place}")
+    print() 
+
+
+
+
+
+
+
+            
+        
+
+
+    
+
+
+
+
+
+
+
